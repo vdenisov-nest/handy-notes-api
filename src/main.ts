3461408d53b3ac46bd439a852b08e51dc38203b5
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-04-18 13:22:31 +0300

    set up environment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import * as config from 'config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const { NODE_ENV } = process.env;
+const { PORT } = config.get('SERVER');
+
+if (NODE_ENV) {
+  // tslint:disable-next-line:no-console
+  console.log(`\n ---------- "${NODE_ENV.toLocaleUpperCase()}" IN PROGRESS ---------- \n`);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // prefix
+  app.setGlobalPrefix('api');
+
+  await app.listen(PORT);
+  Logger.log(`Server running on http://localhost:${PORT}`, 'Bootstrap');
 }
 bootstrap();
