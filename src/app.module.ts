2a268288287ff6b2dfff4320738952ffaefc90e8
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-04-18 13:42:23 +0300

    add to global: filter (http-exception), interceptor (logging), pipe (validation)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import * as config from 'config';
 
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import { HttpExceptionFilter } from './shared/http-exception.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 const DB_PARAMS: any = config.get('DATABASE');
 const { type, host, port, username, password, database } = DB_PARAMS;
@@ -17,6 +21,10 @@ console.log('\n', '=> DB URI =>', dbURI, '\n');
     TypeOrmModule.forRoot({ ...DB_PARAMS }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_FILTER,        useClass: HttpExceptionFilter },
+    { provide: APP_INTERCEPTOR,   useClass: LoggingInterceptor },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as config from 'config';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 const { NODE_ENV } = process.env;
 const { PORT } = config.get('SERVER');
@@ -17,6 +17,14 @@ async function bootstrap() {
 
   // prefix
   app.setGlobalPrefix('api');
+
+  // pipes
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(PORT);
   Logger.log(`Server running on http://localhost:${PORT}`, 'Bootstrap');
