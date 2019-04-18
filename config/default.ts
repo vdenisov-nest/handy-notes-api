1f08a4eb0f22788b90d99d2d52185cdf24261c36
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-04-18 13:33:46 +0300

    configure database connection

--- config/default.ts
+++ config/default.ts
@@ -2,4 +2,15 @@ module.exports = {
   SERVER: {
     PORT: 3000,
   },
+  DATABASE: {
+    type: 'postgres',
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: 5432,
+    username: 'root',
+    password: '1234',
+    database: 'my-storage',
+    synchronize: true,
+    logging: true,
+    entities: ['./src/**/*.entity.ts', './dist/**/*.entity.ts'],
+  },
 };
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
+import * as config from 'config';
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const DB_PARAMS: any = config.get('DATABASE');
+const { type, host, port, username, password, database } = DB_PARAMS;
+const dbURI = `${type}://${username}:${password}@${host}:${port}/${database}`;
+
+// tslint:disable-next-line:no-console
+console.log('\n', '=> DB URI =>', dbURI, '\n');
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({ ...DB_PARAMS }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
